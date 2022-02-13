5250ad91985a513aeac5551302504f533c43b5ce
Mohmmad Altmash <mohmmad@pop-os.localdomain>
2022-02-13 23:00:09 +0530

    first commit

---
+++ database/conn.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Altu@7270";
+$database = "portfolio"; 
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+?>
---
+++ database/help_request.php
@@ -0,0 +1,21 @@
+<?php
+include 'conn.php';
+if(isset($_POST['name']))
+{    
+     $name = $_POST['name'];
+     $email = $_POST['email'];
+     $details = $_POST['details'];
+      echo $name;
+      echo $email;
+      echo $details;
+
+
+     $sql = "INSERT INTO help_request (name,email,details)
+     VALUES ('$name','$email','$details')";
+     if (mysqli_query($conn, $sql)) {
+      header("Location:/Portfolio/index.html");
+     }
+
+     mysqli_close($conn);
+}
+?>
